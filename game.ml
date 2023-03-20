b38b04e7e653706d9ac1da81d897a7ca7a992fe7
Lukman <lom4@cornell.edu>
2023-03-19 22:25:44 -0400

    Started Game.ml

--- game.ml
+++ game.ml
@@ -1,1 +1,10 @@
 open Yojson.Basic.Util
+
+type spot =
+  | CollectPay of { id : int; prompt : string; value : int; next : spot option }
+  | LifeChoice of { id : int; prompt : string; next : spot option }
+
+type board = spot list
+type career = { name : string; salary : int }
+type player = { money : int; career : career; position : spot }
+type t = { current_player : player; players : player list; game_board : board }

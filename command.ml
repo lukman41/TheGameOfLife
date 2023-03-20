e68ae87d853c1dc4ec8549245458ca3f73c25bd8
Joshua Jaquez <jj498@cornell.edu>
2023-03-19 23:13:41 -0400

    Made changes to command.ml
    -implemented parse

--- command.ml
+++ command.ml
@@ -1,1 +1,16 @@
+type object_phrase = string list
 
+type command =
+  | Quit
+  | Spin
+
+exception Empty
+exception Malformed
+
+let parse str = 
+  match List.filter (fun ele -> String.length ele > 0) (String.split_on_char ' ' str) with
+  | [] -> raise Empty
+  | h::t -> if h = "quit" && List.length t = 0 then Quit 
+  else if h = "spin" && List.length t = 0 
+    then Spin else 
+        raise Malformed

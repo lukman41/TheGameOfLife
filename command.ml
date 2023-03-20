b34b427ed967da3aab178d085e591180bf1a6b4a
Lukman <lom4@cornell.edu>
2023-03-20 16:00:15 -0400

    updated command.ml

--- command.ml
+++ command.ml
@@ -3,14 +3,22 @@ type object_phrase = string list
 type command =
   | Quit
   | Spin
+  | Choose of object_phrase
 
 exception Empty
 exception Malformed
 
-let parse str = 
-  match List.filter (fun ele -> String.length ele > 0) (String.split_on_char ' ' str) with
+let parse str =
+  match
+    List.filter
+      (fun ele -> String.length ele > 0)
+      (String.split_on_char ' ' str)
+  with
   | [] -> raise Empty
-  | h::t -> if h = "quit" && List.length t = 0 then Quit 
-  else if h = "spin" && List.length t = 0 
-    then Spin else 
-        raise Malformed
+  | h :: t -> (
+      match (h, t) with
+      | "spin", [] -> Spin
+      | "quit", [] -> Quit
+      | "choose", [] -> raise Empty
+      | "choose", _ -> Choose t
+      | _, _ -> raise Malformed)

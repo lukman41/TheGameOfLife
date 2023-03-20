18d85eeaa3f684438658a664a556124406343ab0
Nadia Barakatain <nnb28@cornell.edu>
2023-03-19 21:57:46 -0400

    Updated example_board json file to account for new board outline
    - replaced previous json file with new outline for the board

--- game.mli
+++ game.mli
@@ -1,1 +1,9 @@
 (*my name is BLeu*)
+type t
+(* the current game *)
+
+val from_json : Yojson.Basic.t -> t
+(** [from_json j] is the board that [j] represents. Requires: [j] is a valid
+    JSON board representation. *)
+
+val get_state : t -> t

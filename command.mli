5d804da75df9c793e0a3303d479192f8058ad573
Joshua Jaquez <jj498@cornell.edu>
2023-03-19 23:05:34 -0400

    Made changes to command.mli

---
+++ command.mli
@@ -0,0 +1,47 @@
+(** Parsing of player commands. *)
+
+type object_phrase = string list
+(** The type [object_phrase] represents the object phrase that can be part of a
+    player command. Each element of the list represents a word of the object
+    phrase, where a "word" is defined as a consecutive sequence of non-space
+    characters. Thus, no element of the list should contain any leading,
+    internal, or trailing spaces. The list is in the same order as the words in
+    the original player command. For example:
+
+    - If the player command is ["quit     "], then the object phrase is
+      [Quit].
+
+    - If the player command is ["  spin     "], then the object phrase is
+      again [Spin].*)
+
+    
+
+(** The type [command] represents a player command that is decomposed into a
+    verb and possibly an object phrase. *)
+
+type command =
+  | Quit
+  | Spin
+
+exception Empty
+(** Raised when an empty command is parsed. *)
+
+exception Malformed
+(** Raised when a malformed command is parsed. *)
+
+val parse : string -> command
+(** [parse str] parses a player's input into a [command], as follows. The first
+    word (i.e., consecutive sequence of non-space characters) of [str] becomes
+    the verb. Examples:
+
+    - [parse "quit"] is [Quit].
+    - [parse "spin"] is [Spin].
+
+    Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space
+    characters (only ASCII character code 32; not tabs or newlines, etc.).
+
+    Raises: [Empty] if [str] is the empty string or contains only spaces.
+
+    Raises: [Malformed] if the command is malformed. A command is malformed if
+    the verb is not "spin", or "quit", if the verb is "quit" or "spin" and there is a
+    non-empty object phrase. *)

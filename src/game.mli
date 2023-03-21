99d50dfab1b7e2d2e643e3db97c5ca404db2b87f
Bleu <aas329@cornell.edu>
2023-03-20 20:48:41 -0400

    Added prompt for all players to spin the wheel

--- src/game.mli
+++ src/game.mli
@@ -13,11 +13,5 @@ val end_game : t -> string
 (** [end_game g] is the name of the winner of the game based on their money
     amounts *)
 
-<<<<<<< HEAD
 val make_player : string -> career option -> player
 (* [make_player n c] takes in a name and a career and returns a player *)
-=======
-val make_player : string -> string -> player
-(* [make_player n c] takes in a name and a career an returns a player at the
-   start of the game *)
->>>>>>> 753e3e6eca8ccfd5235cd9e5ac7d841686b02f50

9796d6ea44612a6e16b935f191c291ac5e24b266
Joshua Jaquez <jj498@cornell.edu>
2023-03-20 21:54:19 -0400

    Made changes to main.ml
    -added more string for the TUI to print

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,15 @@
 open Game_of_life
 open Game
 
+
 let main () =
-  print_string " Welcome to the game of ";
+  ANSITerminal.print_string [ANSITerminal.green]" \n\nWelcome to The Game of ";
   ANSITerminal.print_string [ ANSITerminal.magenta ] "L";
   ANSITerminal.print_string [ ANSITerminal.blue ] "I";
   ANSITerminal.print_string [ ANSITerminal.green ] "F";
   ANSITerminal.print_string [ ANSITerminal.yellow ] "E";
-  print_string ". ";
-  print_endline "Player_1's name: ";
+  print_endline "! ";
+  print_endline "Player_1's name: "; 
   let player_1 = make_player (read_line ()) None in
   print_endline "Player_2's name:";
   let player_2 = make_player (read_line ()) None in
@@ -16,12 +17,14 @@ let main () =
   let player_3 = make_player (read_line ()) None in
   print_endline "Player_4's name:";
   let player_4 = make_player (read_line ()) None in
-  let player_list = [ player_1; player_2; player_3; player_4 ] in
-<<<<<<< HEAD
-  ()
-=======
-  ();
-  print_string "Take turns spinning the wheel. The highest spin will go first."
->>>>>>> 99d50dfab1b7e2d2e643e3db97c5ca404db2b87f
+  let player_list = [ player_1; player_2; player_3; player_4 ] in 
+  print_endline "Take turns spinning the wheel. The highest spin will go first.";
+  print_string " Type '";
+  ANSITerminal.print_string [ ANSITerminal.yellow ] "s";
+  ANSITerminal.print_string [ ANSITerminal.magenta ] "p";
+  ANSITerminal.print_string [ ANSITerminal.blue ] "i";
+  ANSITerminal.print_string [ ANSITerminal.green ] "n";
+  print_string "' to start. "
+  
 
 let () = main ()
--- src/game.mli
+++ src/game.mli
@@ -15,14 +15,11 @@ val end_game : t -> string
 
 val make_player : string -> career option -> player
 (* [make_player n c] takes in a name and a career and returns a player *)
-<<<<<<< HEAD
 
-val player_name : player -> string
-(* [player_name p ] take in a player and returns the name field of the player *)
+(* val player_name : player -> string
+[player_name p ] take in a player and returns the name field of the player *)
 
-val first_turn_spin : player list -> player * player list
+(* val first_turn_spin : player list -> player * player list
 (* [first_turn_spin l p] takes in a player list and after spinning for all
    players returns the player which will go first as well as the list of players
-   in order of their turn *)
-=======
->>>>>>> 99d50dfab1b7e2d2e643e3db97c5ca404db2b87f
+   in order of their turn *) *)

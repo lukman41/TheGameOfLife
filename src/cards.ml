9e1116ece1eaba92a74a1c9d6b0967ebe3bbab81
Nadia Barakatain <nnb28@cornell.edu>
2023-05-11 16:40:52 -0400

    You can draw an action card at random now

--- src/cards.ml
+++ src/cards.ml
@@ -67,6 +67,23 @@ let action_choice_from_json json : action_cards =
       b_amount = json |> member "odd_amount" |> to_int;
     }
 
+let house_from_json json : house_card =
+  {
+    id = json |> member "id" |> to_int;
+    name = json |> member "name" |> to_string;
+    price = json |> member "price" |> to_int;
+    even_amount = json |> member "even_amount" |> to_int;
+    odd_amount = json |> member "odd_amount" |> to_int;
+  }
+
+let career_from_json json : career_card =
+  {
+    id = json |> member "id" |> to_int;
+    name = json |> member "name" |> to_string;
+    salary = json |> member "salary" |> to_int;
+    bonus = json |> member "bonus_salary" |> to_int;
+  }
+
 let action_cards json =
   let action_pay_list =
     json |> member "action_pay" |> to_list |> List.map action_pay_from_json
@@ -79,3 +96,25 @@ let action_cards json =
     |> List.map action_choice_from_json
   in
   action_pay_list @ spin_list @ choice_list
+
+let house_cards json =
+  json |> member "house" |> to_list |> List.map house_from_json
+
+let career_cards json =
+  json |> member "career" |> to_list |> List.map career_from_json
+
+let rec draw_action_card_helper lst index =
+  let h, t =
+    match lst with
+    | [] -> failwith "Invalid action card list for drawing"
+    | h :: t -> (h, t)
+  in
+  match index with
+  | 0 -> h
+  | _ -> draw_action_card_helper t (index - 1)
+
+let draw_action_card l =
+  Random.self_init ();
+  let range = List.length l in
+  let r = Random.int range in
+  draw_action_card_helper l r

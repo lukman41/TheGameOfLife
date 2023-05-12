39beb2e98e007b674263da528294b2411ebece15
Bleu <aas329@cornell.edu>
2023-05-12 00:02:40 -0400

    Edited specifications for board.ml

--- src/board.ml
+++ src/board.ml
@@ -1,13 +1,17 @@
 open Game
 open Yojson.Basic.Util
 
-(**Takes in the spot from the json and returns the spot's type*)
+(**Takes in a single spot from the json and returns the type of spot as a string*)
 let get_spot_type json = json |> member "name" |> to_string
 
-(** Takes in the board json and outputs a list with the spot_types as strings*)
+(** Takes in the board json and outputs it as a list in order with the
+    spot_types as strings*)
 let board_from_json json =
   json |> member "board" |> to_list |> List.map get_spot_type
 
+(**Builds an accumulator for the board as a spot list. Keeps track of the
+   previous spot we just worked with in order to have next when making the
+   current spot*)
 let rec make_board_helper acc prev string_board =
   let backward_board = List.rev string_board in
   match backward_board with
@@ -22,5 +26,8 @@ let rec make_board_helper acc prev string_board =
       let new_spot = make_spot h prev in
       new_spot :: acc
 
+(**Makes the board with an empty accumulator *)
 let make_board board = make_board_helper [] retire_spot board
+
+(**Returns the start of the board *)
 let start_spot board = List.hd board

1b39c56847a2d0defbe101bbb96e4062387af37d
Nadia Barakatain <nnb28@cornell.edu>
2023-05-13 23:10:18 -0400

    added pp_list and pp_string to test_suite

--- test/main.ml
+++ test/main.ml
@@ -3,7 +3,25 @@ open Game_of_life
 open Game
 open Command
 open Cards
-open Board
+open Yojson.Basic.Util
+
+(** [pp_string s] pretty-prints string [s]. *)
+let pp_string s = "\"" ^ s ^ "\""
+
+(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
+    pretty-print each element of [lst]. *)
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [ h ] -> acc ^ pp_elt h
+      | h1 :: (h2 :: t as t') ->
+          if n = 100 then acc ^ "..." (* stop printing long list *)
+          else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
+    in
+    loop 0 "" lst
+  in
+  "[" ^ pp_elts lst ^ "]"
 
 let spin_test (name : string) (expected_output : int) : test =
   name >:: fun _ ->
